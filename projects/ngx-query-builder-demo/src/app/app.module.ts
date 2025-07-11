1f8f5b656c7e548fd456f536231292d832b24c6b
Bill Eisner <eisnerw@hotmail.com>
2025-07-11 22:14:46 +0000

    Temporarily add ngx-query-builder demo

---
+++ projects/ngx-query-builder-demo/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { AppComponent } from './app.component';
+import { EditRulesetDialogComponent } from './edit-ruleset-dialog.component';
+
+import { QueryBuilderModule } from 'ngx-query-builder';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatButtonModule,
+    QueryBuilderModule
+  ],
+  declarations: [ AppComponent, EditRulesetDialogComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+}
